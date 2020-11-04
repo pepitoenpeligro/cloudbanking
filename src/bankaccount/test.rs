b714fc480a8c96f0586a4a7e550e9284b7701f0d
José Antonio Córdoba Gómez <joseinn@correo.ugr.es>
2020-11-04 08:27:33 +0100

    #40 update unit test account

--- src/bankaccount/test.rs
+++ src/bankaccount/test.rs
@@ -26,4 +26,18 @@ mod test_bankaccount{
 
         assert_eq!(*acc.get_id(),String::from("507f1f77bcf86cd799439011"));
     }
+
+
+    #[test]
+    /// Test if swift assignment is correct
+    fn test_get_swift(){
+        let id      : String            = String::from("507f1f77bcf86cd799439011");
+        let swift   : String            = String::from("BSCHESMMXXX");
+        let iban    : String            = String::from("ES326688051111222233344");
+        let date    : NaiveDateTime     = NaiveDate::from_ymd(2020, 7, 8).and_hms(22, 18, 0);
+        let status  : bool              = true;
+        let acc     : Account           = Account::new(id, swift, iban, date, status);
+
+        assert_eq!(*acc.get_swift(),String::from("BSCHESMMXXX"));
+    }
 }
