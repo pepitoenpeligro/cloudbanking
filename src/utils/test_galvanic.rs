baf5130543a96c4137e3a5b19d9e1b14f2af3493
José Antonio Córdoba Gómez <joseinn@correo.ugr.es>
2020-11-07 16:38:39 +0100

    #47 updating test lib with galvanic

---
+++ src/utils/test_galvanic.rs
@@ -0,0 +1,37 @@
+mod test_galvanic_utils{
+    use galvanic_assert::*;
+    use galvanic_assert::matchers::*;
+
+    use chrono::{NaiveDate, NaiveDateTime};
+    use crate::bankaccount::model::bankaccount::*;
+    use crate::bankcard::model::{Card};
+    use crate::utils::model::{Lib};
+
+    #[test]
+    /// Test if a swift code is valid or not
+    fn test_check_valid_swift(){
+        let new_swift   : String            = String::from("BBVAESMMXXX");
+        let bad_swift   : String            = String::from("BVAEMMX");
+
+        assert_that!(&Lib::check_swift(new_swift), is(eq(true)));
+        assert_that!(&Lib::check_swift(bad_swift), is(eq(false)));
+    }
+
+    #[test]
+    /// Test if a swift code is valid or not
+    fn test_check_valid_iban(){
+        let new_iban   : String            = String::from("ES49998877666655554440");
+        let german_iban   : String         = String::from("DE63250500000201414513");
+        let bad_iban   : String            = String::from("XXX632505000002014145");
+
+        assert_that!(&Lib::check_iban(new_iban), is(eq(true)));
+        assert_that!(&Lib::check_iban(german_iban), is(eq(true)));
+        assert_that!(&Lib::check_iban(bad_iban), is(eq(false)));
+
+    }
+
+}
+
+
+
+
