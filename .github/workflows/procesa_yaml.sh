fc8a0a352d719047b1b916fd707f74f8480a75f9
JJ Merelo <jjmerelo@gmail.com>
2020-12-06 19:30:46 +0100

    A mi me funciona

--- .github/workflows/procesa_yaml.sh
+++ .github/workflows/procesa_yaml.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 echo "Procesando fichero cc.yaml"
-cc=$(ysh -f cc.yaml)
-ysh -T ${cc} -q "lenguaje"
-lenguaje=$(ysh -T ${cc} -q "lenguaje")
-echo $lenguaje
-fichero_tareas=$(ysh -T ${cc} -q "fichero_tareas")
-echo $fichero_tareas
+lenguaje=$(ysh -f cc.yaml -q "lenguaje")
+[[ $lenguaje =~ \= ]] && echo "«lenguaje» no es una cadena" && exit 1
+fichero_tareas=$(ysh -f cc.yaml -q "fichero_tareas")
+[[ $fichero_tareas =~ \= ]] && echo "«fichero_tareas» no es una cadena" && exit 2
 echo "CC_LENGUAJE=$lenguaje" >> CC_CONFIG
 echo "CC_FICHERO_TAREAS=$fichero_tareas" >> CC_CONFIG
