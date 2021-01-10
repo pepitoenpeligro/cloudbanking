3ad9c0c8eec2bb37ee4ac4f714cddc824d0d12e4
José Antonio Córdoba Gómez <joseinn@correo.ugr.es>
2021-01-10 20:19:22 +0100

    Close #74 Close #75 documentation about additional work and advance

--- .github/workflows/procesa_yaml.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Procesando fichero cc.yaml"
-lenguaje=$(ysh -f cc.yaml -q "lenguaje")
-[[ $lenguaje =~ \= ]] && echo "«lenguaje» no es una cadena" && exit 1
-fichero_tareas=$(ysh -f cc.yaml -q "fichero_tareas")
-[[ $fichero_tareas =~ \= ]] && echo "«fichero_tareas» no es una cadena" && exit 2
-echo "CC_LENGUAJE=$lenguaje" >> CC_CONFIG
-echo "CC_FICHERO_TAREAS=$fichero_tareas" >> CC_CONFIG
