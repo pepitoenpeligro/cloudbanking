c5fd6bd6a095cdcd50bac937c9088d178e90b55c
José Antonio Córdoba Gómez <joseinn@correo.ugr.es>
2021-02-02 00:58:54 +0100

    Fix #81 update loans. request to api done

--- client/src/core/BankLoans.js
+++ client/src/core/BankLoans.js
@@ -12,50 +12,100 @@ const BankLoans = ({history}) => {
         units: [],
         unitsVisible : false,
         testnow:'Test Now!',
-        nombreUnidades:[]
+        nombreUnidades:[],
+        loans: [],
+        loansVisible : false
         
 
         
      });
  
-    const {units , unitsVisible, testnow, nombreUnidades} = values;
+    const {units , unitsVisible, testnow, nombreUnidades, loans, loansVisible} = values;
  
 
      React.useEffect(() => {
-         
-        //  axios({
-        //      method: 'POST',
-        //      url: `${process.env.REACT_APP_API}/units/list`,
-        //      headers:{
-        //         'Content-Type': 'application/json',
-        //         'Accept':'application/json'
-        //     }}).then(function(response ){
-        //         console.log("[/units/list] Received:", response.data.listUnidadesName);
-        //         setValues(values => ({...values, nombreUnidades: response.data.listUnidadesName}))
-        //     })
+
+        axios({
+            method: 'GET',
+            url: `${process.env.REACT_APP_API}/loans`,
+            headers:{
+                'Content-Type': 'application/json',
+                'Accept':'application/json'
+            }}
+            ).then( function(response){
+                console.log(response);
+                console.log(response.data);
+                setValues(values => ({...values, loans: response.data, loansVisible:true}))
+                console.log("Loans object")
+                console.log(loans);
+                toast.success('Your bank loans have been recovered')
+            })
         }, [])
 
 
 
-
-
-
-    const handleTestUnit = (item) =>  (event) => {
-        history.push(   {pathname: '/testunits',
-                        state:item})
-        console.log("EL boton ve:", item)
-
-    }
-
-
-    const handleTestExam = (name) => (event) => {
-        toast.success('A por el test', name)
-    }
-
     const generateBankLoansView = (event) => {
+
+        if (!loansVisible){
+            return (
+                <p>NO Cargado</p>
+            )
+        }else{
+            console.log("Estare bien?");
+            console.log(loans);
+            return (
+                <div>
+                <p>YAAAa</p>
+                
+                
+                    {/* {loansVisible && loans.map(function(d, idx){
+                        return (<li key={idx}>{d._id}</li>)
+                    })} */}
+
+
+
+                    <table>
+                        {/* <tr key={"header"}>
+                            {Object.keys(state[0]).map((key) => (
+                            <th>{key}</th>
+                            ))}
+                        </tr> */}
+                        {loans.map((item) => (
+                            <tr key={item._id}>
+                            {Object.values(item).map((val) => (
+                                <td>{val}</td>
+                            ))}
+                            </tr>
+                        ))}
+                        </table>
+
+
+
+
+
+                </div>
+                
+            )
+        }
+
         return(
             <div>
-                <p>Here you can see all bank loans</p>
+                {/* <p>{Here you can see all bank loans}</p> */}
+                <p>{loansVisible && JSON.stringify(loans)}</p>
+
+                <div>
+                    {loansVisible && loans.map(function(d, idx){
+                        return (<li key={idx}>{d.name}</li>)
+                    })}
+                    </div>
+
+                {/* {loansVisible && 
+                <ul>
+                    {loans.map(l => {
+                    return <li>{l._id}</li>;
+                    })
+                    }
+                </ul>} */}
             </div>
         )
     }
