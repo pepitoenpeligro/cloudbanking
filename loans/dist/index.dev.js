580f0f5c3f0cc92fc74af3792af6c1dad04b13a1
José Antonio Córdoba Gómez <joseinn@correo.ugr.es>
2021-02-02 17:23:41 +0100

    Fix #81 remove double cors

--- loans/dist/index.dev.js
+++ loans/dist/index.dev.js
@@ -1,8 +1,14 @@
 "use strict";
 
+var _mongoose$connect;
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
 var express = require("express");
 
 var app = express();
+
+var mongoose = require('mongoose');
 
 var bodyParser = require('body-parser');
 
@@ -18,11 +24,24 @@ var _process$env = process.env,
     MONGO_HOSTNAME = _process$env.MONGO_HOSTNAME,
     MONGO_PORT = _process$env.MONGO_PORT,
     MONGO_DB = _process$env.MONGO_DB;
-var url = "mongodb://".concat(MONGO_USERNAME, ":").concat(MONGO_PASSWORD, "@").concat(MONGO_HOSTNAME, ":").concat(MONGO_PORT, "/").concat(MONGO_DB, "?authSource=admin");
-mongoose.connect(url, {
-  useNewUrlParser: true
+var url = "mongodb://".concat(MONGO_USERNAME, ":").concat(MONGO_PASSWORD, "@").concat(MONGO_HOSTNAME, ":").concat(MONGO_PORT, "/").concat(MONGO_DB, "?authSource=admin&retryWrites=true&w=majority&ssl=false");
+mongoose.connect(url, (_mongoose$connect = {
+  useNewUrlParser: true,
+  useFindAndModify: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+}, _defineProperty(_mongoose$connect, "useFindAndModify", false), _defineProperty(_mongoose$connect, "autoCreate", true), _mongoose$connect)).then(function (val) {
+  console.log("DB connected");
+  console.log(mongoose.connection.readyState);
+})["catch"](function (err) {
+  return console.error("DB Connection errror", err);
 });
 app.use(bodyParser.json());
+app.use(function (req, res, next) {
+  // res.header("Access-Control-Allow-Origin", "*");
+  // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use('/loans', dataRoutes);
 app.use(function (request, response) {
   response.status(400);
--- loans/index.js
+++ loans/index.js
@@ -32,8 +32,8 @@ mongoose.connect(url, {
 app.use(bodyParser.json());
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    // res.header("Access-Control-Allow-Origin", "*");
+    // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
 
